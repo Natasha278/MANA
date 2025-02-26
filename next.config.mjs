77cd719d50835e3812d3a9bd6cf06130b6f5590f
Natasha278 <102701456+Natasha278@users.noreply.github.com>
2025-02-26 22:13:37 +0100

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,37 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'coolbackgrounds.io',
+      },
+      {
+        protocol: 'https',
+        hostname: 'ui.shadcn.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.pexels.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'static.vecteezy.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.pinimg.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.publicdomainpictures.net',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
